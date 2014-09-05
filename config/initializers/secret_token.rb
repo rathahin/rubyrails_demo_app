494a57ed3eac1a7884393298dd16ad81bfec051d
Ratha Hin <rh@goldengekko.com>
2014-09-05 17:28:39 +0700

    Initial commit
    Change-Id: I2ccec41e5daa3b4040f6853602e22c7168b1ac2d

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '562a317e66448102f1b73172c869d065c3ab6af410b286f8edb539f1c369a49bcee0026d80f752ad6ae89995c7830aebebe4ba6922100c1f8588129930c8b9e6'
